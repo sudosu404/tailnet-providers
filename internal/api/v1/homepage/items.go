0edad7377a10b07ba95f34692e487db43d257421
yusing <yusing@6uo.me>
2025-09-04 06:31:44 +0800

    feat(homepage): implement SearchRoute method and enhance item configuration with sorting and visibility features, introduce All and Favorite categories

--- internal/api/v1/homepage/items.go
+++ internal/api/v1/homepage/items.go
@@ -1,16 +1,23 @@
 package homepageapi
 
 import (
+	"fmt"
 	"net/http"
+	"net/url"
+	"slices"
+	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lithammer/fuzzysearch/fuzzy"
 	apitypes "github.com/yusing/go-proxy/internal/api/types"
+	"github.com/yusing/go-proxy/internal/homepage"
 	"github.com/yusing/go-proxy/internal/route/routes"
 )
 
 type HomepageItemsRequest struct {
-	Category string `form:"category" validate:"omitempty"`
-	Provider string `form:"provider" validate:"omitempty"`
+	SearchQuery string `form:"search" validate:"omitempty"`
+	Category    string `form:"category" validate:"omitempty"`
+	Provider    string `form:"provider" validate:"omitempty"`
 } //	@name	HomepageItemsRequest
 
 // @x-id				"items"
@@ -20,6 +27,7 @@ type HomepageItemsRequest struct {
 // @Tags			homepage
 // @Accept			json
 // @Produce		json
+// @Param			search		query		string	false	"Search query"
 // @Param			category	query		string	false	"Category filter"
 // @Param			provider	query		string	false	"Provider filter"
 // @Success		200			{object}	homepage.Homepage
@@ -42,5 +50,75 @@ func Items(c *gin.Context) {
 		hostname = host
 	}
 
-	c.JSON(http.StatusOK, routes.HomepageItems(proto, hostname, request.Category, request.Provider))
+	c.JSON(http.StatusOK, HomepageItems(proto, hostname, &request))
+}
+
+func HomepageItems(proto, hostname string, request *HomepageItemsRequest) homepage.Homepage {
+	switch proto {
+	case "http", "https":
+	default:
+		proto = "http"
+	}
+
+	hp := homepage.NewHomepageMap(routes.HTTP.Size())
+
+	if strings.Count(hostname, ".") > 1 {
+		_, hostname, _ = strings.Cut(hostname, ".") // remove the subdomain
+	}
+
+	for _, r := range routes.HTTP.Iter {
+		if request.Provider != "" && r.ProviderName() != request.Provider {
+			continue
+		}
+		item := r.HomepageItem()
+		if request.Category != "" && item.Category != request.Category {
+			continue
+		}
+		if request.SearchQuery != "" && !fuzzy.MatchFold(request.SearchQuery, item.Name) {
+			continue
+		}
+
+		// clear url if invalid
+		_, err := url.Parse(item.URL)
+		if err != nil {
+			item.URL = ""
+		}
+
+		// append hostname if provided and only if alias is not FQDN
+		if hostname != "" && item.URL == "" {
+			isFQDNAlias := strings.Contains(item.Alias, ".")
+			if !isFQDNAlias {
+				item.URL = fmt.Sprintf("%s://%s.%s", proto, item.Alias, hostname)
+			} else {
+				item.URL = fmt.Sprintf("%s://%s", proto, item.Alias)
+			}
+		}
+
+		// prepend protocol if not exists
+		if !strings.HasPrefix(item.URL, "http://") && !strings.HasPrefix(item.URL, "https://") {
+			item.URL = fmt.Sprintf("%s://%s", proto, item.URL)
+		}
+
+		hp.Add(&item)
+	}
+
+	ret := hp.Values()
+	// sort items in each category
+	for _, category := range ret {
+		category.Sort()
+	}
+	// sort categories
+	overrides := homepage.GetOverrideConfig()
+	slices.SortStableFunc(ret, func(a, b *homepage.Category) int {
+		// if category is "Hidden", move it to the end of the list
+		if a.Name == homepage.CategoryHidden {
+			return 1
+		}
+		if b.Name == homepage.CategoryHidden {
+			return -1
+		}
+		// sort categories by order in config
+		return overrides.CategoryOrder[a.Name] - overrides.CategoryOrder[b.Name]
+	})
+	return ret
 }
