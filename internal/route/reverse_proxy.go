653765ff7b00cfe9122b97c6dcf6c05b86a7c750
yusing <yusing@6uo.me>
2025-09-07 11:15:35 +0800

    fix: prevent panicking on misconfigurations

--- internal/route/reverse_proxy.go
+++ internal/route/reverse_proxy.go
@@ -175,13 +175,13 @@ func (r *ReveseProxyRoute) addToLoadBalancer(parent task.Parent) {
 		_ = lb.Start(parent) // always return nil
 		linked = &ReveseProxyRoute{
 			Route: &Route{
-				Alias:     cfg.Link,
-				Homepage:  r.Homepage,
-				HealthMon: lb,
+				Alias:    cfg.Link,
+				Homepage: r.Homepage,
 			},
 			loadBalancer: lb,
 			handler:      lb,
 		}
+		linked.SetHealthMonitor(lb)
 		routes.HTTP.AddKey(cfg.Link, linked)
 		r.task.OnFinished("remove_loadbalancer_route", func() {
 			routes.HTTP.DelKey(cfg.Link)
